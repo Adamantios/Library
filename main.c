e02e84e7da1ba12ed57ab2cbcc4c2cdfaaabb5ee
manos <adamantioszaras@gmail.com>
2017-05-13 16:35:17 +0300

    Correct main functions.

--- main.c
+++ main.c
@@ -17,6 +17,9 @@ void addBookTest(char *filename, list bList) {
     // add the new book at the book list.
     addBook(*newBook, bList);
 
+    // save changes to the file.
+    save(filename, bList);
+
     // initialise a second book.
     strcpy(newBook->author, "Someone");
     strcpy(newBook->title, "His Book");
@@ -47,6 +50,9 @@ void findBookTest(list bList) {
     // find the book with id 1 from the list.
     *bookToSearch = findBook(*bookToSearch, bList);
 
+    // free the memory for the book.
+    free(bookToSearch);
+
     // print the book that was found.
     printf("Printing the book with id 1...\n");
     print(*bookToSearch);
@@ -57,7 +63,7 @@ void deleteBookTest(char *filename, list bList) {
     book *bookToDelete = (book *) malloc(sizeof(book));
 
     // set the book's id to 1.
-    bookToDelete->id = 2;
+    bookToDelete->id = 1;
 
     // delete the book with id 1 from the list.
     if (deleteBook(*bookToDelete, bList))
@@ -70,16 +76,27 @@ void deleteBookTest(char *filename, list bList) {
         printf("Printing the book menu after deleting the book with id 1...\n");
         printMenu();
     }
+
+    // free the memory for the book.
+    free(bookToDelete);
 }
 
 void updateBookTest(char *filename, list bList) {
     // allocate memory for the book.
     book *bookToUpdate = (book *) malloc(sizeof(book));
 
-    // set the book's id to 1.
-    bookToUpdate->id = 1;
+    // set the book's id to 2.
+    bookToUpdate->id = 2;
 
-    // update the book with id 1 from the list.
+    // change the information of the book.
+    strcpy(bookToUpdate->author, "Someone Else");
+    strcpy(bookToUpdate->title, "The Best Book");
+    bookToUpdate->genre = FICTION;
+    strcpy(bookToUpdate->reviews[0], "First Review.");
+    strcpy(bookToUpdate->reviews[1], "Second Review.");
+    strcpy(bookToUpdate->reviews[2], "Third Review.");
+
+    // update the book with id 2 from the list.
     if (updateBook(*bookToUpdate, bList))
         printf("Something went wrong while trying to update the book...\n");
     else {
@@ -90,6 +107,9 @@ void updateBookTest(char *filename, list bList) {
         printf("Printing the book menu after updating the book with id 1...\n");
         printMenu();
     }
+
+    // free the memory for the book.
+    free(bookToUpdate);
 }
 
 void startTesting(char *filename) {
