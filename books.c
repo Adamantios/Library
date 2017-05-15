382e81296148bbc772492259eb129ccf82c9ef3a
manos <adamantioszaras@gmail.com>
2017-05-15 20:41:44 +0300

    Add TODO.

--- books.c
+++ books.c
@@ -190,6 +190,7 @@ int updateBook(book b, list bList) {
         currentBook = currentBook->next;
 
     if (currentBook != NULL) {
+        // TODO pass one by one the values.
         currentBook->book = b;
         return 0;
     } else
